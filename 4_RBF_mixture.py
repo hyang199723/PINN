f219ef890f18af43799a06dd208e69a2c91801ee
Hongjian Yang <hoyang@espgpu01.unx.sas.com>
2023-12-21 10:40:56 -0500

    mixture

--- 4_RBF_mixture.py
+++ 4_RBF_mixture.py
@@ -20,7 +20,7 @@ else:
 import scipy.stats as stats
 import pylab
 
-#%% Simulate a 2-D log normal
+#%% Simulate a 2-D stationary data
 N = 1000
 P = 2
 noise_var = 0.1
@@ -28,7 +28,8 @@ rho = 0.2
 nu = 1
 kappa = (8 * nu)**(0.5) / rho
 spatial_var = 1
-X, Y = gen_lognormal(N, rho, spatial_var, noise_var, nu)
+X, Y = gen_mixture(N, spatial_var, noise_var, nu1 = 3.5, nu2 = 0.5,
+                   rho1 = 0.3, rho2 = 0.1)
 X = X[:, 1:3] # Only need coors
 # RBF centers
 num_basis = [3,5,6,8]
@@ -69,8 +70,7 @@ plt.scatter(X_test[:, 0], X_test[:, 1], s = 20, c = y0_model1)
 model1_mse = np.mean((y_test - y0_model1)**2)
 plt.title(f'Predicted value; MSE = {model1_mse}')
 # %% Replicates
-alphas = [100, 256, 1000]
-#[0, 0.5, 1, 2, 4, 8, 16, 32, 64]#
+alphas = [0, 0.5, 1, 2, 4, 8, 16, 32, 64, 100, 256, 1000]
 iters = 5
 MSE = pd.DataFrame(data = 0.0, index = range(iters), columns = alphas)
 for idx, alpha in enumerate(alphas):
